d07b594131eb5a0761e8ec61cf4a7291099d11b2
Pedro Bohrer <pedrombohrer@gmail.com>
2024-01-11 14:55:28 -0300

    first

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let total = 0
+let carrinho = document.getElementById("lista-produtos");
+function adicionar() {
+    //adiciona produtos ao carrinho de compras com sua quantidade
+    //calcula valor total da compra
+    let produto = document.getElementById("produto");
+    let nomeProduto = produto.value.split("-")[0];
+    let quantidade = document.getElementById("quantidade").value == 0 ? 1 : document.getElementById("quantidade").value;
+    let valorProduto = parseInt(produto.value.match(/\d+/g));
+    let valorProdutoQuant = valorProduto * quantidade;
+
+    total = total + valorProdutoQuant;
+    document.getElementById("valor-total").innerHTML = `R$${total}`;
+
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span>
+  </section>`
+    
+}
+function limpar() {
+    //limpa carrinho de compra e total das compras
+    total = 0
+    document.getElementById("valor-total").innerHTML = `R$${total}`;
+    carrinho.innerHTML = ""
+}
